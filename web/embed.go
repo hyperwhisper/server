8f8452b755b91cafc51e1f7432dec0705a2d43f4
Ameya Shenoy <shenoy.ameya@gmail.com>
2026-01-16 00:40:54 +0530

    fix: prod docker builds
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- web/embed.go
+++ web/embed.go
@@ -6,6 +6,7 @@ import "embed"
 
 // DistFS embeds the built frontend static files.
 // The dist folder is created by running `bun run generate` in the web directory.
+// Using all: prefix to include _nuxt directory (go:embed skips _ prefixed dirs by default)
 //
-//go:embed dist
+//go:embed all:dist
 var DistFS embed.FS
